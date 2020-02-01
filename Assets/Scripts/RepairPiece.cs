a964c40acc60ffff21a2d9b0e466a1a4daa62a3e
ChangesInBold <33079131+ChangesInBold@users.noreply.github.com>
2020-01-31 21:53:16 -0500

    Add files via upload

---
+++ Assets/Scripts/RepairPiece.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairPiece : MonoBehaviour
+{
+    public enum RepairType
+    {
+        Leg,
+        Arm,
+        Head
+    }
+
+    public bool IsHeld = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
